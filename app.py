870bcd7a9e96a724f8b3dce9915fa3a8f813b61b
Rod5919 <sergiorodrigofernandeztesta@gmail.com>
2023-07-15 19:01:06 -0400

    Entrada y salida de datos

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import json
 
 app = Flask(__name__)
+app.secret_key = "mysecretkey"
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', title='Restaurante de comida rápida')
+
+@app.route('/forms', methods=['GET', 'POST'])
+def forms():
+    if request.method == 'GET':
+        return render_template('form.html', title='Pedido')
+    if request.method == 'POST':
+        print(request.form)
+        with open('data/db.json', 'r') as file:
+            data = json.load(file)
+            data.append(request.form)
+        with open('data/db.json', 'w') as file:
+            json.dump(data, file, indent=4)
+        flash('Datos enviados correctamente')
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
